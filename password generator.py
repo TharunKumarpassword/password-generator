9f055fc171c7c35c8ce614f5170c950968be7278
TharunKumarpassword <tharun0062@gmail.com>
2025-06-12 19:56:45 +0530

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,7 @@
+a=input("Enter new password:")
+print("Password Generated")
+b=input("Enter Password To Unlock Your Device:")
+if a==b:
+    print("Your Device Has Been Unlocked")
+else:
+    print("Incorrect password")
